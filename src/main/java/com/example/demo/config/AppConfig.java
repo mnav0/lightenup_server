408dc9fb7a24ca8224afd1a24e02be3fedba06d6
Maggie Navracruz <mnavracruz@gmail.com>
2022-03-25 15:02:45 -0400

    changed timeout

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -12,6 +12,6 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
 
-        return restTemplateBuilder.setConnectTimeout(Duration.ofSeconds(5)).setReadTimeout(Duration.ofSeconds(45)).build();
+        return restTemplateBuilder.setConnectTimeout(Duration.ofSeconds(15)).setReadTimeout(Duration.ofSeconds(50)).build();
     }
 }
